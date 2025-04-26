ed77130214c34e67929fdeb5f86a4af787e39455
Facundo Gonzales <facundogonzalesliendo@gmail.com>
2025-04-26 20:50:29 -0300

    se arma tabla para ver resultados

---
+++ src/app/shared/table/table.component.ts
@@ -0,0 +1,47 @@
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+
+import { TranslateModule } from '@ngx-translate/core';
+
+import { Character } from '../../models/character-model';
+
+@Component({
+  selector: 'app-table',
+  standalone: true,
+  imports: [
+    CommonModule,
+    TranslateModule,
+    MatTableModule,
+    MatPaginatorModule,
+  ],
+  templateUrl: './table.component.html',
+  styleUrl: './table.component.scss'
+})
+export class TableComponent  implements OnChanges  {
+  @Input() dataSource: Character[] = [];
+  @Input() totalResults: number = 0;
+
+  @Output() pageChanged = new EventEmitter<PageEvent>();
+  @Output() detailCharacter = new EventEmitter<Character>();
+
+  displayedColumns: string[] = ['name', 'status', 'species', 'created'];
+  dataSourceTable = new MatTableDataSource<Character>([]);
+  pageSize: number = 20;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['dataSource']) {
+      this.dataSourceTable.data = this.dataSource;
+    }
+  }
+
+  onPageChange(event: PageEvent): void {
+    this.pageChanged.emit(event);
+  }
+
+  viewItem(character: Character): void {
+    this.detailCharacter.emit(character);
+  }
+}
