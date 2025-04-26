75f4c13aef5da9d5a25dcfda880cdd187d736ccc
Facundo Gonzales <facundogonzalesliendo@gmail.com>
2025-04-26 17:22:21 -0300

    se arman rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/pages.component').then(p => p.PagesComponent)
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./pages/pages.component').then(p => p.PagesComponent),
+    pathMatch: 'full'
+  }
+];
