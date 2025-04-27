a3da1dfe60aaa4aac40a388106b188a9e35d6177
Facundo Gonzales <facundogonzalesliendo@gmail.com>
2025-04-27 10:59:51 -0300

    se arma visualizacion de favoritos

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,18 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+import { FavoriteStore } from '../../stores/favorite.store';
+import { Character } from '../../models/character-model';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [CommonModule, TranslateModule, MatBadgeModule, MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  // @Input() title: string = '';
+  @Output() viewDetailCharacter = new EventEmitter<Character>();
 
-  constructor(public translate: TranslateService) {
+  private translate = inject(TranslateService);
+  private favoriteStore = inject(FavoriteStore);
+  favorites$ = this.favoriteStore.favorites$;
+
+  constructor() {
     this.translate.setDefaultLang('es');
     this.translate.use('es');
+  }
+
+  viewDetail(character: Character): void {
+    this.viewDetailCharacter.emit(character)
   }
 }
