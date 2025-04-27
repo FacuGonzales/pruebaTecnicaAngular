83f13a8132b1458cda31b9e8786b1140cdab38bd
Facundo Gonzales <facundogonzalesliendo@gmail.com>
2025-04-27 20:51:13 -0300

    se mejora e integra graphql en componente de detalles

--- src/app/shared/detail-character/detail-character.component.ts
+++ src/app/shared/detail-character/detail-character.component.ts
@@ -1,74 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject, Input, SimpleChanges } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
+
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { ApolloQueryResult } from '@apollo/client/core';
+
 import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 
 import { Character } from '../../models/character-model';
 import { CharactersDataService } from '../../services/characters-data.service';
-import { catchError, Observable, of } from 'rxjs';
-import { Episode } from '../../models/episode-model';
-import { CommonModule } from '@angular/common';
-import { Residents } from '../../models/residents-model';
+import { CharacterDetails } from '../../models/character-detail-model';
 
 @Component({
   selector: 'app-detail-character',
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, TranslateModule, MatCardModule, MatListModule, MatDividerModule],
   templateUrl: './detail-character.component.html',
   styleUrl: './detail-character.component.scss'
 })
 export class DetailCharacterComponent {
-  // private characterService = inject(CharactersDataService);
+  private translate = inject(TranslateService);
   @Input() character!: Character;
 
-  episodes$!: Observable<Episode>;
+  characterDetails$!: Observable<ApolloQueryResult<CharacterDetails> | null>;
 
-  residentsOrigin: Character[] = [];
-  residentsLocation: Character[] = [];
-
-
-  constructor(private characterService: CharactersDataService) {}
+  constructor(private characterService: CharactersDataService) {
+    this.translate.setDefaultLang('es');
+    this.translate.use('es');
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['character']) {
-      this.episodes$ = this.characterService.getEpisodes(this.character.episode[0]);
-      this.getResidentsOrigin();
-      this.getResidentsLocation();
-    }
-  }
-
-  getResidentsOrigin(): void {
-    if (this.character.origin?.url) {
-      return this.loadResidents(this.character.origin.url, true, false);
-    }
-  }
-
-  getResidentsLocation(): void {
-    if (this.character.location?.url) {
-      return this.loadResidents(this.character.location.url, false, true);
-    }
-  }
-
-
-  loadResidents(url: string, origin: boolean, location: boolean): void {
-    this.characterService.getResidents(url)
-    .pipe(catchError(() => of(null)))
-    .subscribe(data => {
-      if (data?.residents && data.residents.length > 0) {
-
-        // LIMITAMOS a máximo 5 residentes
-        const maxResidents = data.residents.slice(0, 5);
-
-        maxResidents.forEach(_url => {
-          this.characterService.getCharacterByUrl(_url)
-            .pipe(catchError(() => of(null)))
-            .subscribe(resident => {
-              if (resident) {
-                if(origin) this.residentsOrigin.push(resident);
-
-                if(location) this.residentsLocation.push(resident);
-              }
-            });
-        });
+      if (this.character) {
+        this.loadCharacterDetails(this.character.id);
       }
-    })
+    }
   }
 
+  loadCharacterDetails(characterId: number): void {
+    this.characterDetails$ = this.characterService.getCharacterDetails(characterId).pipe(
+      catchError(() => of(null))
+    );
+  }
 }
