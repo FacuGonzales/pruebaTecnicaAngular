f88fdcce9a782ad0bf721005f756af17177e4dcd
Facundo Gonzales <facundogonzalesliendo@gmail.com>
2025-04-27 20:53:11 -0300

    se arma metodos en services para consumo de rest y graphql

--- src/app/services/characters-data.service.ts
+++ src/app/services/characters-data.service.ts
@@ -1,9 +1,12 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Episode } from '../models/episode-model';
-import { Residents } from '../models/residents-model';
-import { Character } from '../models/character-model';
+
+import { Apollo } from 'apollo-angular';
+import { ApolloQueryResult } from '@apollo/client/core';
+
+import { GET_CHARACTER_DETAILS } from '../constants/querys.constants';
+import { CharacterDetails } from '../models/character-detail-model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +14,7 @@ import { Character } from '../models/character-model';
 export class CharactersDataService {
   private apiUrl = 'https://rickandmortyapi.com/api/character';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private apollo: Apollo) {}
 
   getCharactersList(name: string = '', status: string = '', page: number = 1): Observable<any> {
     let params = new HttpParams();
@@ -25,17 +28,10 @@ export class CharactersDataService {
     return this.http.get<any>(this.apiUrl, { params });
   }
 
-
-  getEpisodes(api: string): Observable<Episode> {
-    return this.http.get<Episode>(api);
-  }
-
-  getResidents(api: string): Observable<Residents> {
-    return this.http.get<Residents>(api);
-  }
-
-
-  getCharacterByUrl(api: string): Observable<Character> {
-    return this.http.get<Character>(api);
+  getCharacterDetails(characterId: number): Observable<ApolloQueryResult<CharacterDetails>> {
+    return this.apollo.watchQuery<CharacterDetails>({
+      query: GET_CHARACTER_DETAILS,
+      variables: { id: characterId },
+    }).valueChanges;
   }
 }
