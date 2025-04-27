e5e6983aad7dbbd9c3e60a3d05e9bd4a61fa43cc
Facundo Gonzales <facundogonzalesliendo@gmail.com>
2025-04-26 22:56:00 -0300

    se arman metodos services

--- src/app/services/characters-data.service.ts
+++ src/app/services/characters-data.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Episode } from '../models/episode-model';
+import { Residents } from '../models/residents-model';
+import { Character } from '../models/character-model';
 
 @Injectable({
   providedIn: 'root'
@@ -22,4 +25,17 @@ export class CharactersDataService {
     return this.http.get<any>(this.apiUrl, { params });
   }
 
+
+  getEpisodes(api: string): Observable<Episode> {
+    return this.http.get<Episode>(api);
+  }
+
+  getResidents(api: string): Observable<Residents> {
+    return this.http.get<Residents>(api);
+  }
+
+
+  getCharacterByUrl(api: string): Observable<Character> {
+    return this.http.get<Character>(api);
+  }
 }
