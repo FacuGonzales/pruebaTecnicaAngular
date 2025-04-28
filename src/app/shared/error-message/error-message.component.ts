bdc508109e96bb2ab6a9abd3a05eaae2ccb8525e
Facundo Gonzales <facundogonzalesliendo@gmail.com>
2025-04-28 12:09:05 -0300

    se arman estilos para error component

--- src/app/shared/error-message/error-message.component.ts
+++ src/app/shared/error-message/error-message.component.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-error-message',
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, MatIconModule],
   templateUrl: './error-message.component.html',
   styleUrl: './error-message.component.scss'
 })
 export class ErrorMessageComponent {
   private translate = inject(TranslateService);
+  @Output() backToSearch: EventEmitter<boolean> = new EventEmitter();
 
   constructor() {
     this.translate.setDefaultLang('es');
