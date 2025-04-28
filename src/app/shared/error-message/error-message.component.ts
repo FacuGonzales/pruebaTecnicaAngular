2f8f7f60293d79a677b5f1066ed3866ec60862db
Facundo Gonzales <facundogonzalesliendo@gmail.com>
2025-04-28 09:41:50 -0300

    se importa translate

--- src/app/shared/error-message/error-message.component.ts
+++ src/app/shared/error-message/error-message.component.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-error-message',
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './error-message.component.html',
   styleUrl: './error-message.component.scss'
 })
 export class ErrorMessageComponent {
+  private translate = inject(TranslateService);
 
+  constructor() {
+    this.translate.setDefaultLang('es');
+    this.translate.use('es');
+  }
 }
