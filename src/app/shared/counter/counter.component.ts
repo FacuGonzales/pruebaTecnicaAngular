c6923fafe0d91581a35d9eac1387a105effc0d19
Facundo Gonzales <facundogonzalesliendo@gmail.com>
2025-04-28 19:50:18 -0300

    se crea componente para mostrar contadores

---
+++ src/app/shared/counter/counter.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.scss'
+})
+export class CounterComponent {
+  @Input() data: any;
+}
